d2e253947a458318a984b3283115603f57dcda82
harrycg <39111936+harrycg@users.noreply.github.com>
2018-05-12 00:16:38 +0930

    Update test.rb

--- test.rb
+++ test.rb
@@ -3,12 +3,32 @@ require 'nationbuilder'
 client = NationBuilder::Client.new('harrycossar', ENV['NATIONBUILDER_APIKEY'], retries: 8)
 
   puts "Loading donations..."
+response = client.call(:people, :index)
+
+
+paginated = NationBuilder::Paginator.new(client, response)
+
+ people = paginated.body["results"]
+
+people.each do |person|
+    email = person['email']
+    first_name = person['first_name']
+    last_name = person['last_name']
+  id = person['id']
+
+  
+    puts "#{email} #{first_name} #{last_name} #{id}"
+
+
+  
 params = {
-id: 15,
+id: "#{id}",
   tagging: {
-    tag: "is: awesome core 2018"
+    tag: "is: community 2018"
   }
   
 }
 
-  client.call(:people, :tag_person , params)
+  client.call(:people, :bulk_tag_removal , params)
+  
+end
