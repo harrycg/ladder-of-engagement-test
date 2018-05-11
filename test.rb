12d914e3088e9bfe6712b3f6899dd812b4542b95
harrycg <39111936+harrycg@users.noreply.github.com>
2018-05-11 15:08:22 +0930

    Update test.rb

--- test.rb
+++ test.rb
@@ -2,6 +2,7 @@ require 'nationbuilder'
 
 client = NationBuilder::Client.new('aycc', ENV['NATIONBUILDER_APIKEY'], retries: 8)
 
-  
+  puts "Loading donations..."
+
   client.raw_call("/people/#14/taggings", "put", {"tagging": {"tag": ["harry_test_cleanup2"]}})
   
