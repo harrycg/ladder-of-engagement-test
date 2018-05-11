4f40b9355320e95bf6e8e57c02e9ab87f475e953
harrycg <39111936+harrycg@users.noreply.github.com>
2018-05-11 23:07:28 +0930

    Update test.rb

--- test.rb
+++ test.rb
@@ -3,15 +3,20 @@ require 'nationbuilder'
 client = NationBuilder::Client.new('harrycossar', ENV['NATIONBUILDER_APIKEY'], retries: 8)
 
   puts "Loading donations..."
-params = {
-id: 15,
+  params = {
   tagging: {
-    tag: "is: community 2018"
+    tag:["is: awesome core 2018", "is: community 2018"]
+  }
   }
   
-}
+  response = client.call(:people, :match , params)
 
-  client.call(:people, :tag_person , params)
+
+paginated = NationBuilder::Paginator.new(client, response)
+page1 = paginated
+page2 = page1.next
+page3 = page2.next
+
 
   puts "HELLO"
 
