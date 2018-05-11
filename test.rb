f85e66793800bad1b52fd3f200c3e4bc30fcc168
harrycg <39111936+harrycg@users.noreply.github.com>
2018-05-11 15:41:40 +0930

    Update test.rb

--- test.rb
+++ test.rb
@@ -3,10 +3,15 @@ require 'nationbuilder'
 client = NationBuilder::Client.new('harrycossar', ENV['NATIONBUILDER_APIKEY'], retries: 8)
 
   puts "Loading donations..."
+params = {
+  person: {
+    email: "bob@example.com",
+    last_name: "Smith",
+    first_name: "Bob"
+  }
+}
 
-client.call(:people, :destroy, id: 14)
-
-
+client.call(:people, :create, params)
 
   puts "HELLO"
 
