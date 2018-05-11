2a8f7195552aa20e2f1dd7656b6f018e929dec1e
harrycg <39111936+harrycg@users.noreply.github.com>
2018-05-11 16:38:54 +0930

    Update test.rb

--- test.rb
+++ test.rb
@@ -3,7 +3,8 @@ require 'nationbuilder'
 client = NationBuilder::Client.new('harrycossar', ENV['NATIONBUILDER_APIKEY'], retries: 8)
 
   puts "Loading donations..."
-  client.call("https://harrycossar.nationbuilder.com/api/v1/people/15/taggings", "put", {"tagging": {"tag": "is: community 2018"}})
+  client.call(:people, :put {"tagging": {"tag": "is: community 2018"}}, id: 15)
+
 
   puts "HELLO"
 
