06d0a9426d037289ae564740b05d2ef98166970e
harrycg <39111936+harrycg@users.noreply.github.com>
2018-06-05 17:38:13 +0930

    Update test.rb

--- test.rb
+++ test.rb
@@ -7,7 +7,10 @@ uri = URI.parse("https://slack.com/api/chat.postMessage")
 request = Net::HTTP::Post.new(uri)
 request.content_type = "application/json; charset=UTF-8"
 request["Authorization"] = "Bearer ENV['SLACK_APIKEY']"
-request.body="{channel:techteam, text:Hope}"
+request.body= JSON.dump({
+  "channel" => "techteam", 
+  "text" => "Hope"
+  })
 
 req_options ={
   use_ssl: uri.scheme == "https",
