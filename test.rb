94bd077dfba870abc90d1386efdf7e050e83f44f
harrycg <39111936+harrycg@users.noreply.github.com>
2018-06-05 22:36:36 +0930

    Update test.rb

--- test.rb
+++ test.rb
@@ -1,3 +1,52 @@
+require 'nationbuilder'
+
+client = NationBuilder::Client.new('harrycossar', ENV['NATIONBUILDER_APIKEY'], retries: 8)
+
+puts "Loading people with tag is: awesome core q2 2018"
+
+filter = {
+  tag: "is:%20awesome%20core%20q2%202018"
+  }
+  
+response = client.call(:people_tags, :people, filter)
+
+page = NationBuilder::Paginator.new(client, response)
+
+people = page.body['results']
+page = page.next
+
+people = []
+while page.prev?
+  people += page.body['results']
+  page = page.next
+end  
+
+
+people.each do |person|
+  email = person['email']
+    first_name = person['first_name']
+    last_name = person['last_name']
+  id = person['id']
+  
+puts "#{first_name} #{last_name}"
+
+
+params = {
+ id: "#{id}",
+  tagging: {
+    tag: "is: community 2018"
+  }
+  
+}
+  client.call(:people, :bulk_tag_removal , params)
+
+end
+
+=begin
+    client.call(:people, :tag_person , params3)
+
+=end
+
 =begin
 require 'net/http'
     require 'json'
